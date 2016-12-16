1ec2b05ff729a255dc8075e6f421c1c8a8c8c7cd
Juan <00jarg@gmail.com>
2016-12-16 15:07:30 -0300

    Yeah! We have a main!

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ *  pgn2fen - Extracts FEN of a specific move on a PGN game
+ *  -------------------------------------------------------
+ *  Author: mate_amargo
+ *  https://github.com/mate-amargo/pgn2fen
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv) {
+
+
+
+	exit(EXIT_SUCCESS);
+
+}
