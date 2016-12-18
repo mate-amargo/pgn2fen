370ee9bd604a035e822c158170fb310830750892
Juan <00jarg@gmail.com>
2016-12-18 18:24:20 -0300

    Pawn pushes

--- main.c
+++ main.c
@@ -18,6 +18,12 @@
 #define NARGSOPT 2	/* Optional arguments */
 #define FILES 8			/* Board files (columns) (a-h) */
 #define RANKS 8			/* Board ranks (rows) (1-8) */
+#define K (1 << 3)	/* White can castle Kingside */
+#define Q (1 << 2)	/* White can castle Queenside */
+#define k (1 << 1)	/* Black can castle Kingside */
+#define q 1 				/* Black can castle Queenside */
+#define WHITE 1			/* Used to determine which turn... */
+#define BLACK 0			/* ...is whilst traversing the list of moves */
 
 int main (int argc, char **argv) {
 
@@ -99,7 +105,7 @@ int main (int argc, char **argv) {
 	list->next = NULL;
 	x = list;
 
-	int i; /* Why didn't we have one of these before?? */
+	int i, j; /* Why didn't we have one of these before?? */
 	char c, d;	/* Buffers to hold chars */
 	int breakout = 0, breakout2 = 0; /* flags to breakout of while loops from within switch statements */
 	int ply = 1; /* "ply" is the half-move number in the pgn file. "move" is the argument. */
@@ -129,7 +135,7 @@ int main (int argc, char **argv) {
 					break;
 				case '1':	case '2':	case '3':	case '4':	case '5':	case '6':	case '7':	case '8': case '9':
 					breakout2 = 0;
-					/* Distinguish between move numbers and things like e4xd5 */
+					/* Distinguish between move numbers and things like R2xf4 */
 					d = c; /* Keep the old number in hand */
 					while (!breakout2 && (c = fgetc(finput)) != EOF) { /* If it's a number keep eating chars */
 						switch (c) {
@@ -173,13 +179,78 @@ int main (int argc, char **argv) {
 		ply++;
 	}
 
-	if(feof(finput)) {
+	if (feof(finput)) {
 		printf("*** Error: Move number %d by %s does not exist\n", move, (side == 'w')?"white":"black");
 		exit(EXIT_FAILURE);
 	}
 
-	x = list;
-	while (x) { printf("%s ", x->move); x = x->next; } printf("\n");
+	/* Now we have the list of moves loaded into "list" */
+
+	/* We need to set up a board to record the game as it progress */
+	/* This is the structure we'll use to display the first field of the FEN output */
+	char board[RANKS][FILES] = {
+		/* 8 */ {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+
+		/* 7 */ {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+
+		/* 6 */ {'1', '1', '1', '1', '1', '1', '1', '1'},
+
+		/* 5 */ {'1', '1', '1', '1', '1', '1', '1', '1'},
+
+		/* 4 */ {'1', '1', '1', '1', '1', '1', '1', '1'},
+
+		/* 3 */ {'1', '1', '1', '1', '1', '1', '1', '1'},
+
+		/* 2 */ {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+
+		/* 1 */ {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+
+		/*        a    b    c    d    e    f    g    h        */	
+	};
+
+	char castling = 17; /* Third field of the FEN: KQkq, each letter represents a bit, thus 17 is all castling allowed */
+	int turn = WHITE;
+
+	x = list->next; /* Skip the empty item */
+	while (x) {
+	//	for (i = 0; x->move[i] != '\0'; i++)
+//								printf("\"%s\"\n", x->move);	
+		switch (x->move[0]) {
+			case 'a':	case 'b':	case 'c':	case 'd':	case 'e':	case 'f':	case 'g':	case 'h': /* Pawn move */
+				if (strlen(x->move) > 2) { /* Move with capture */
+					
+					/* Parenthesis are important, otherwise because RANKS is an int, the chars will get promoted and we'll get a wrong result */
+					board[RANKS - (x->move[2] - '0')][x->move[1] - 'a'] = (turn)?'P':'p';
+				} else {
+					if (turn && '4' == x->move[1] && board[6][x->move[0] - 'a'] == 'P') /* The pawn could've came from white's first move */
+						board[6][x->move[0] - 'a'] = '1';
+					else if (!turn && '5' == x->move[1] && board[1][x->move[0] - 'a'] == 'p') /* The pawn could've came from black's first move */
+						board[1][x->move[0] - 'a'] = '1';
+					else if (turn) /* White pawn push */
+						board[RANKS - (x->move[1] - '0') + 1][x->move[0] - 'a'] = '1';
+					else /* Black pawn push */
+						board[RANKS - (x->move[1] - '0') - 1][x->move[0] - 'a'] = '1';
+					board[RANKS - (x->move[1] - '0')][x->move[0] - 'a'] = (turn)?'P':'p';
+				}
+				break;
+			case 'R':	case 'N':	case 'B': case 'Q': case 'K':
+			case 'O':
+				break;
+		}
+		x = x->next;
+		turn = (turn)?BLACK:WHITE; /* Toggle turn */
+	}
+
+	/* Print the first field of the FEN */
+	for (i = 0; i < RANKS-1; i++) {
+		for (j = 0; j < FILES; j++)
+			fprintf(foutput, "%c", board[i][j]);
+		fprintf(foutput, "/");
+	}
+	/* The last rank doesn't have "/", so print it now */
+	for (i = RANKS-1, j = 0; j < FILES; j++)
+		fprintf(foutput, "%c", board[i][j]);
+
 
 	exit(EXIT_SUCCESS);
 
